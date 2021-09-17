abf3abb410fd73773978e12507ce760d095c365b
ousszizou <odjaidri@gmail.com>
2021-09-17 23:41:21 +0100

    (laravel8 & vue2) lesson4 - add tailwindcss

--- webapp/src/main.js
+++ webapp/src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import AppLayout from "@/layouts/AppLayout";
+import "tailwindcss/tailwind.css";
 
 Vue.config.productionTip = false;
 
